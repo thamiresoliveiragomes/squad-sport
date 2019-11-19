fa00bf951169932ef852f3abf0b56ae04c9f65c4
thamiresoliveiragomes <thamiresoliveira1004@gmail.com>
2019-11-19 19:18:28 -0300

    adicionado filtros

--- src/app.js
+++ src/app.js
@@ -24,21 +24,5 @@ function pageRoute() {
   };    
 };
 
-
-
-// var url = 'https://newsapi.org/v2/everything?' +
-//           'q=esportes&' +
-//           'language=pt&' +
-//           'sortBy=popularity&' +
-//           'apiKey=d16f33243081454ba1f24d06cbbeffe3';
-
-// var req = new Request(url);
-
-// fetch(req)
-//     .then(res =>res.json())
-//     .then(data => console.log(data))
-
-
 window.addEventListener('load', pageRoute);
 window.addEventListener('hashchange', pageRoute, false);
-
--- src/components/button.js
+++ src/components/button.js
@@ -2,8 +2,10 @@ function Button(props) {
   const template = `
     <button 
     data-id="${props.dataId}"
+    id="${props.id}"
     class="${props.class} primary-button"
     onclick="button.handleClick(event, ${props.onClick})" >${props.title}
+    <i class="${props.icone}"></i>
     </button>
   `;
   return template;
@@ -12,7 +14,7 @@ function Button(props) {
 window.button = {
   handleClick: (event, callback) => {
     event.preventDefault();
-    callback(event);
+    callback(event.target.textContent);
   },
   component: Button,
 };
