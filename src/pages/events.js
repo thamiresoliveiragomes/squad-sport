7788c80f651c871cb10413ea094282a93f23bc8c
seu-catarinabarbosa <catarina.pinheiro07@gmail.com>
2019-11-19 00:00:39 -0300

    Reestruturacao do template cards

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,17 +3,9 @@ function printEvent(post) {
   const eventList = document.querySelector(".eventos");
   const date = post
     .data()
-    .date.toDate()
-    .toLocaleString("pt-BR");
-<<<<<<< HEAD
-  const descricao = post.data().descrição;
-  const esporte = post.data().esporte;
-  const evento = post.data().evento;
-  const genero = post.data().gênero;
-  const imagem = post.data().imagem;
-  const localizacao = post.data().localização;
-  const valor = post.data().valor;
-=======
+    .date;
+    // .toDate()
+    // .toLocaleString("pt-BR");
   const additional = post.data().additional;
   const sport = post.data().sport;
   const event = post.data().event;
@@ -22,82 +14,67 @@ function printEvent(post) {
   const address = post.data().address;
   const price = post.data().price;
 
->>>>>>> 1c9f3a018e74261324044432e2c5e3ed905cd082
   const template = `
-    <section class ='card-events'>
-    <img src= '${image}'/>
-    <p>
-    Evento: ${event}
-    </p>
-    <p>
-    Data: ${date}
-    </p>
-    <p>
-<<<<<<< HEAD
-    Modaliade: ${esporte}
-=======
-    Modaliade: ${sport} 
->>>>>>> 1c9f3a018e74261324044432e2c5e3ed905cd082
-    </p>
-    <p>
-    Gênero: ${gender}
-    </p>
-    <p>
-    Localização: ${address}
-    </p>
-    <p>
-    Valor(es): ${price}
-    </p>
-    <p>
-    Sobre o evento: ${additional}
-    </p>
-    </section>
-    `
+    <li class ='card-event'>
+        <div class='card-event-img'>
+            <img src= '${image}'/>
+        </div>
+        <div class='card-event-text'>
+        <p>
+            Evento: ${event} <br />
+            Data: ${date} <br />
+            Modaliade: ${sport} <br />
+            Gênero: ${gender} <br />
+            Localização: ${address} <br />
+            Valor(es): ${price} <br />
+            Sobre o evento: ${additional}
+        </p>
+        </div>
+    </li>
+    `;
   eventList.innerHTML += template;
 }
 function loadEvent() {
   const postCollection = firebase.firestore().collection("events");
-  postCollection.get().then(snap => {
-      snap.forEach(post => {
+  postCollection.orderBy('date', 'desc').get().then(snap => {
+    snap.forEach(post => {
       printEvent(post);
     });
   });
 }
 function Publicize() {
-    window.location.hash = '#publicize';
+  window.location.hash = "#publicize";
 }
 function News() {
-    window.location.hash = '#news';
+  window.location.hash = "#news";
 }
 function Events() {
-    const template = `
+  const template = `
+    <div class='container-events'>
         <ul class="eventos"></ul>
+        <div class="container-buttons">
         ${Button({
-            id:'news',
-            title: 'Notícias',
-            onClick: News,
+            id: "news",
+            title: "Notícias",
+            onClick: News
         })}
         ${Button({
-            id:'publicize',
-            title: 'Divulgue seu evento',
-            onClick: Publicize,
+            id: "publicize",
+            title: "Divulgue seu evento",
+            onClick: Publicize
         })}
-        ${Button({ 
-          class: 'js-genero',
-          id:'filter',
-          title: 'Filtros',
-          // onClick: templateFilter,
+        ${Button({
+            class: "js-genero",
+            id: "filter",
+            title: "Filtros"
+            // onClick: templateFilter,
         })}
-        
+        </div>
+    </div>
     `;
-    return template;
+  return template;
 }
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> 1c9f3a018e74261324044432e2c5e3ed905cd082
 window.app = {
   loadEvent,
   printEvent
